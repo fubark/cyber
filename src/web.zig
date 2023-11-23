0f724c1b9261d0dc36476e9152081e5d0e2aae79
fubark <fubar.web@gmail.com>
2023-11-23 14:09:39 -0500

    Reimplement cbindgen using clang.
    Fix

--- src/web.zig
+++ src/web.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const c = @import("clib.zig");
 const cy = @import("cyber.zig");
 
 comptime {
@@ -14,26 +15,29 @@ export fn csSetupForWeb(vm: *cy.UserVM) void {
     vm.setPrint(print);
 }
 
-pub fn loader(uvm: *cy.UserVM, spec_: cy.Str, out: *cy.ModuleLoaderResult) callconv(.C) bool {
-    const spec = spec_.slice();
+pub fn loader(vm: ?*c.VM, spec_: c.Str, out_: [*c]c.ModuleLoaderResult) callconv(.C) bool {
+    const out: *c.ModuleLoaderResult = out_;
+    const spec = c.strSlice(spec_);
     if (std.mem.eql(u8, "web", spec)) {
         const src = (
             \\--| Evals JS from the host environment.
             \\@host func eval(val any) none
         );
-        out.* = .{
+        out.* = std.mem.zeroInit(c.ModuleLoaderResult, .{
             .src = src,
             .srcLen = src.len,
             .funcLoader = funcLoader,
-        };
+        });
         return true;
     } else {
-        return cy.vm_compiler.defaultModuleLoader(uvm, spec_, out);
+        return cy.vm_compiler.defaultModuleLoader(vm, spec_, out);
     }
 }
 
-pub fn funcLoader(_: *cy.UserVM, func: cy.HostFuncInfo, out: *cy.HostFuncResult) callconv(.C) bool {
-    if (std.mem.eql(u8, "eval", func.name.slice())) {
+pub fn funcLoader(_: ?*c.VM, func: c.FuncInfo, out_: [*c]c.FuncResult) callconv(.C) bool {
+    const out: *c.FuncResult = out_;
+    const name = c.strSlice(func.name);
+    if (std.mem.eql(u8, "eval", name)) {
         out.ptr = @ptrCast(&eval);
         return true;
     }
@@ -41,13 +45,13 @@ pub fn funcLoader(_: *cy.UserVM, func: cy.HostFuncInfo, out: *cy.HostFuncResult)
 }
 
 extern fn hostPrint(str: [*]const u8, strLen: usize) void;
-fn print(_: *cy.UserVM, str: cy.Str) callconv(.C) void {
+fn print(_: ?*c.VM, str: c.Str) callconv(.C) void {
     hostPrint(str.buf, str.len);
 }
 
 extern fn hostEvalJS(ptr: [*]const u8, len: usize) void;
-pub fn eval(vm: *cy.UserVM, args: [*]const cy.Value, _: u8) linksection(cy.StdSection) cy.Value {
-    const str = vm.valueToTempString(args[0]);
+pub fn eval(vm: *cy.VM, args: [*]const cy.Value, _: u8) linksection(cy.StdSection) cy.Value {
+    const str = vm.getOrBufPrintValueStr(&cy.tempBuf, args[0]) catch cy.fatal();
     hostEvalJS(str.ptr, str.len);
     return cy.Value.None;
 }
